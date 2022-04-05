1a7e6179041af2e4f2e3849b8d847aa57ca76ef9
Dylan Kynoch <dylan@workn.com>
2022-04-05 16:44:31 -0400

    Reversed parallel attempt.

--- app.py
+++ app.py
@@ -2,70 +2,82 @@
 !pip install numpy
 !pip install pandas
 !pip install pandas_datareader
-!pip install sklearn.linear_model
 '''
-
+import math
+import multiprocessing as mp
 from flask import Flask, request
 import numpy as np
 import pandas as pd
-# import pandas_datareader as web
-import datetime as dt
 import json
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('api/v1/auto_dcf', methods=['POST'])
+@app.route('/api/v1/auto_dcf', methods=['POST', 'GET'])
 def auto_dcf():
-    print(request.json)
-    years = ['2020A', '2021B', '2022P', '2023P', '2024P', '2025P']
-    sales = pd.Series(index=years)
-    sales['2018A'] = 31.0
-
-    growth_rate = 0.1
-    for year in range(1, 6):
-        sales[year] = sales[year - 1] * (1 + growth_rate)
-
-    ebitda_margin = 0.14
-    depr_percent = 0.032
-    ebitda = sales * ebitda_margin
-    depreciation = sales * depr_percent
-    ebit = ebitda - depreciation
-    nwc_percent = 0.24
-    nwc = sales * nwc_percent
-    change_in_nwc = nwc.shift(1) - nwc
-    capex_percent = depr_percent
-    capex = -(sales * capex_percent)
-    tax_rate = 0.25
-    tax_payment = -ebit * tax_rate
-    tax_payment = tax_payment.apply(lambda x: min(x, 0))
-    free_cash_flow = ebit + depreciation + tax_payment + capex + change_in_nwc
-
-    cost_of_capital = 0.12
-    terminal_growth = 0.02
-    terminal_value = ((free_cash_flow[-1] * (1 + terminal_growth)) / (cost_of_capital - terminal_growth))
-    discount_factors = [(1 / (1 + cost_of_capital)) ** i for i in range(1, 6)]
-    dcf_value = (sum(free_cash_flow[1:] * discount_factors) + terminal_value * discount_factors[-1])
-
-    output = pd.DataFrame([sales, ebit, free_cash_flow],
-                          index=['Sales', 'EBIT', 'Free Cash Flow']).round(1)
-
+    """
+    Endpoint for performing the Monte-Carlo Automated Discounted Cash Flow.
+    Requires a json payload with all the variables included.
+    :return: json dump of DCF performed.
+    """
     iterations = 10000
-    mcs = run_mcs(iterations, sales, depr_percent, capex_percent, tax_rate, cost_of_capital)
-    return mcs.to_json()
+    years = ['2020A', '2021B', '2022P', '2023P', '2024P', '2025P']
+    sales_growth = 0.1
+    depr_percent = 0.032
+    capex_percent = depr_percent
+    ebitda_margin = 0.14
+    tax_rate = 0.25
+    nwc_percent = 0.24
+    cost_of_capital = 0.12
 
-
-def run_mcs(iterations, sales, depr_percent, capex_percent, tax_rate, cost_of_capital):
     # Create probability distributions
-    sales_growth_dist = np.random.normal(loc=0.1, scale=0.01, size=iterations)
-    ebitda_margin_dist = np.random.normal(loc=0.14, scale=0.02, size=iterations)
-    nwc_percent_dist = np.random.normal(loc=0.24, scale=0.01, size=iterations)
+    sales_growth_dist = np.random.normal(loc=sales_growth, scale=0.01, size=iterations)
+    ebitda_margin_dist = np.random.normal(loc=ebitda_margin, scale=0.02, size=iterations)
+    nwc_percent_dist = np.random.normal(loc=nwc_percent, scale=0.01, size=iterations)
 
-    # Calculate DCF value for each set of random inputs
+    sales = pd.Series(index=years)
+    sales['2020A'] = 31.0
+
+    results = run_mcs(0, iterations, sales, sales_growth_dist, ebitda_margin_dist, nwc_percent_dist, depr_percent,
+                      capex_percent, tax_rate, cost_of_capital)
+
+    '''
+    print("Number of processors: ", mp.cpu_count())
+        pool = mp.Pool(mp.cpu_count())
+    
+        results = [pool.apply(run_mcs, args=(count * math.ceil(iterations / mp.cpu_count()),
+                                             (count + 1) * math.ceil(iterations / mp.cpu_count()), sales, sales_growth_dist,
+                                             ebitda_margin_dist, nwc_percent_dist,
+                                             depr_percent,
+                                             capex_percent,
+                                             tax_rate,
+                                             cost_of_capital)) for count in range(mp.cpu_count() - 1)]
+    
+        pool.close()
+    '''
+    return json.dumps(results)
+
+
+def run_mcs(start, end, sales, sales_growth_dist, ebitda_margin_dist, nwc_percent_dist, depr_percent, capex_percent,
+            tax_rate, cost_of_capital):
+    """
+    Runs a Monte-Carlo Simulation using the given parameters for DCF.
+    :param start: starting index of series
+    :param end: ending index of series
+    :param sales: pandas dataframe with years as index
+    :param sales_growth_dist: probability distribution for sales growth
+    :param ebitda_margin_dist: probability distribution for ebitda margin
+    :param nwc_percent_dist: probability distribution for net-weighted_capital percent
+    :param depr_percent: float
+    :param capex_percent: float
+    :param tax_rate: float
+    :param cost_of_capital: float
+    :return: list of each iteration valuation
+    """
     output_distribution = []
-    for i in range(iterations):
-        for year in range(1, 6):
+    for i in range(start, end):
+        for year in range(1, len(year)):
             sales[year] = sales[year - 1] * (1 + sales_growth_dist[0])
         ebitda = sales * ebitda_margin_dist[i]
         depreciation = (sales * depr_percent)
@@ -80,10 +92,9 @@ def run_mcs(iterations, sales, depr_percent, capex_percent, tax_rate, cost_of_ca
         # DCF valuation
         terminal_value = (free_cash_flow[-1] * 1.02) / (cost_of_capital - 0.02)
         free_cash_flow[-1] += terminal_value
-        discount_factors = [(1 / (1 + cost_of_capital)) ** i for i in range(1, 6)]
+        discount_factors = [(1 / (1 + cost_of_capital)) ** i for i in range(1, len(year))]
         dcf_value = sum(free_cash_flow[1:] * discount_factors)
         output_distribution.append(dcf_value)
-
     return output_distribution
 
 
